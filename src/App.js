8996bd2684bf879edc14a5f140b3e8d3e944f2b8
Shubham Dalvi <shubham.dalvi008@gmail.com>
2022-09-10 14:24:13 +0530

    finished solution

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import "./App.css";
+import img from "./images/image-qr-code.png";
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello</h1>
+      <div className="card">
+        <div className="card-img">
+          <img src={img} alt="QR code" />
+        </div>
+        <div className="card-text">
+          <h3>Improve your front-end skills by building projects</h3>
+          <p>
+            Scan the QR code to visit Frontend Mentor and take your coding
+            skills to the next level
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
